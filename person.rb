f87fc893f6f44facd6079794365228340d0102f0
Javier <javidx@gmail.com>
2023-04-17 18:07:24 +0300

    Fixed all lint issues

--- person.rb
+++ person.rb
@@ -1,19 +1,25 @@
 class Person
-  attr_reader :id, :name, :age
-  attr_writer :name, :age
-  def initialize(id,age,name='Unknown',parent_permission=true)
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def initialize(id, age, name = 'Unknown', parent_permission: true)
     @id = id
     @name = name
     @age = age
-  end
-
-  private def of_age?
-    return true if @age >= 18
-    false
+    @parent_permission = parent_permission
   end
 
   def can_use_services?
     return true if of_age? || @parent_permission
+
+    false
+  end
+
+  private
+
+  def of_age?
+    return true if @age >= 18
+
     false
   end
 end
