8d72fc68dddc3777c073021cdc575078ee8cd847
Javier <javidx@gmail.com>
2023-04-27 16:46:21 +0300

    Added rspec tests for classroom and rentals

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'pry', '~> 0.13.1'
 gem 'rubocop', '>= 1.0', '< 2.0'
+gem 'rspec', '~> 3.12.0'
---
+++ spec/spec_utils.rb
@@ -0,0 +1,10 @@
+require_relative '../lib/decorators'
+require_relative '../lib/save_decorators'
+require_relative '../lib/rentals'
+require_relative '../lib/classroom'
+require_relative '../lib/student'
+require_relative '../lib/teacher'
+require_relative '../lib/person'
+require_relative '../lib/book'
+
+require 'json'
