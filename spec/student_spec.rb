505e1ea5825f672bf9f4be3f289d5eabcf2e1fa4
Ayo Moses <ayomoses111@gmail.com>
2023-04-28 10:41:45 +0100

    Created unit test for Student class

---
+++ spec/student_spec.rb
@@ -0,0 +1,19 @@
+require './student'
+
+describe Student do
+  context 'create a student' do
+    student = Student.new('Chukwuemeka', 48, 'ENG121', parent_permission: true)
+
+    it 'show name' do
+      expect(student.name).to eq 'Chukwuemeka'
+    end
+
+    it 'show classroom' do
+      expect(student.classroom).to eq 'ENG121'
+    end
+
+    it 'show age' do
+      expect(student.age).to eq 48
+    end
+  end
+end
